897938c9547f2d64370adfc5a094c8ffe67deac4
GnoM <gnomovi4@gmail.com>
2023-05-17 18:19:22 +0300

    add ip output

--- main.tf
+++ main.tf
@@ -9,3 +9,10 @@ resource "aws_instance" "example"{
   tags                   = {"Name" = "Terraform"}
   vpc_security_group_ids = ["sg-0a62d8f422094f3b6"]
 }
+output "instance_public_ip" {
+  value = aws_instance.example.public_ip
+}
+
+output "instance_private_ip" {
+  value = aws_instance.example.private_ip
+}
