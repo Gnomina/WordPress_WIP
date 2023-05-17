86146fe4eb08d4a074ba617a7b2ed8ef6edeac90
GnoM <gnomovi4@gmail.com>
2023-05-17 20:24:49 +0300

    ,

--- main.tf
+++ main.tf
@@ -1,28 +1,19 @@
 provider "aws" {
   region                 = "eu-central-1"
 }
+data "external" "git-branch" {
+    program = ["/bin/bash", "-c", "jq -n --arg branch `git rev-parse --abbrev-ref HEAD` '{\"branch\":$branch}'"]
+}
 
 data "external" "git-branch" {
   program = ["bash", "-c", "echo '{\"branch\":\"'$(git rev-parse --abbrev-ref HEAD)'\"}'"]
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-0d5095d28a904a729"
-  instance_type = "t2.micro"
-  key_name      = "WebAcademy_SSH_Key"
-
-  provisioner "local-exec" {
-    command = "echo 'Branch-${data.external.git-branch.result.branch}-Commit-'$(git rev-parse --short HEAD)"
-    environment = {
-      BRANCH = data.external.git-branch.result.branch
-    }
-    on_failure = ["echo Failure: $BRANCH"]
-  }
-
-  tags = {
-    "Name" = "Branch-${data.external.git-branch.result.branch}-Commit-${chomp(`git rev-parse --short HEAD`)}"
-  }
-
+resource "aws_instance" "example"{
+  ami                    = "ami-0d5095d28a904a729"
+  instance_type          = "t2.micro"
+  key_name               = "WebAcademy_SSH_Key"
+  tags                   = {"Name" = "Terraform"}
   vpc_security_group_ids = ["sg-0a62d8f422094f3b6"]
 }
 output "instance_public_ip" {
--- terraform/main.tf
+++ terraform/main.tf
@@ -9,3 +9,18 @@ resource "aws_instance" "example"{
   tags                   = {"Name" = "Terraform"}
   vpc_security_group_ids = ["sg-0a62d8f422094f3b6"]
 }
+
+output "instance_public_ip" {
+  value = aws_instance.example.public_ip
+ }
+ 
+resource "null_resource" "save_instance_ip" {
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > instance_public_ip.txt"
+    //command = "echo ${aws_instance.example.private_ip} >> instance_public_ip.txt"
+  }
+}
+
+//output "instance_private_ip" {
+//  value = aws_instance.example.private_ip
+//}
