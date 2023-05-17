bc0fba101fc4209e0c7870867c5507c6236cb563
GnoM <gnomovi4@gmail.com>
2023-05-17 19:18:49 +0300

    test commit name

--- main.tf
+++ main.tf
@@ -6,7 +6,11 @@ resource "aws_instance" "example"{
   ami                    = "ami-0d5095d28a904a729"
   instance_type          = "t2.micro"
   key_name               = "WebAcademy_SSH_Key"
-  tags                   = {"Name" = "Terraform"}
+  //tags                   = {"Name" = "Terraform"}
+  tags = {
+    "Name"        = "Terraform"
+    "Branch"      = "${chomp(`git rev-parse --abbrev-ref HEAD`)}"
+    "Commit"      = "${chomp(`git rev-parse --short HEAD`)}"
   vpc_security_group_ids = ["sg-0a62d8f422094f3b6"]
 }
 output "instance_public_ip" {
@@ -15,9 +19,10 @@ output "instance_public_ip" {
  resource "null_resource" "save_instance_ip" {
   provisioner "local-exec" {
     command = "echo ${aws_instance.example.public_ip} > instance_public_ip.txt"
+    //command = "echo ${aws_instance.example.private_ip} >> instance_public_ip.txt"
   }
 }
 
-output "instance_private_ip" {
-  value = aws_instance.example.private_ip
-}
+//output "instance_private_ip" {
+//  value = aws_instance.example.private_ip
+//}
