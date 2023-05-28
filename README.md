796d97657c4d4876c06efb47f693a6a6a06424e1
GnoM <gnomovi4@gmail.com>
2023-05-28 22:50:29 +0300

    install_file readme_update

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # WordPress_WIP_
+
+# Install packages(Jenkins, Ansible, Terraform, Docker)
+
+# clone repo (repo private, need key)
+git clone https://github.com/Gnomina/WordPress_WIP.git
+
+# move to DIR
+cd WordPress_WIP 
+
+# change branche
+git checkout install
+
+# change execute mode
+sudo chmod +x package_install.sh
+
+# run bachscript
+./package_install.sh
--- package_install.sh
+++ package_install.sh
@@ -1,21 +1,59 @@
 #!/bin/bash
 
-
+#-------------------------jenkins installation-------------------------#
 sudo apt-get update
-sudo apt-get install openjdk-11-jdk
+sudo apt-get install openjdk-11-jdk -y
 
-curl -fsSLhttps://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
-    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 	
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-    https://pkg.jenkins.io/debian binary/ | sudo tee \
-    /etc/apt/sources.list.d/jenkins.list > /dev/null	
-
-
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 sudo apt-get update && sudo apt upgrade
-sudo apt-get install jenkins
+sudo apt-get install jenkins -y
 
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
 sudo systemctl status jenkins
+
+#-------------------------ansible installation-------------------------#
+
+sudo apt-get update
+sudo apt-get install software-properties-common -y
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-get install ansible -y
+sudo systemctl enable ansible
+
+#-------------------------Terrform installation-------------------------#
+
+TERRAFORM_VERSION="1.4.6"
+TERRAFORM_URL="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip"
+TERRAFORM_TMP_DIR="/tmp/terraform"
+
+wget $TERRAFORM_URL
+unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+
+rm -rf $TERRAFORM_TMP_DIR
+sudo systemctl enable terraform
+
+#-------------------------Docker installation-------------------------#
+
+sudo apt-get update
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo usermod -aG docker $USER
+sudo systemctl enable docker
+
+
+java --version
+jenkins --version
+ansible --version
+terraform --version
+docker --version
+
