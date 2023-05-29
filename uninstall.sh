0af945721e4e825de1412b7b7d9e0b668d67262b
Gnomina <60029541+Gnomina@users.noreply.github.com>
2023-05-29 21:39:38 +0300

    erase_no_need_files

--- uninstall.sh
+++ uninstall.sh
@@ -53,9 +53,15 @@ echo "--------------------Docker uninstallation--------------------"
 echo " "
 if command -v docker &> /dev/null; then
     echo "Package installed. Uninstalling package"
+
+    sudo systemctl stop docker
+    sudo systemctl disable docker
+    sudo systemctl stop docker.socket
+    sudo systemctl disable docker.socket
     
     sudo apt-get remove --purge docker-ce docker-ce-cli containerd.io -y
     sudo apt-get autoremove -y
+    
     
     echo "Package uninstalled"
 else
